3978048c2a889c22369fbf92ecdd89d2dff1b226
trinibuzz <trinibuzzallmedia@gmail.com>
2026-04-23 22:16:13 -0400

    Fix database username

--- lib/db.ts
+++ lib/db.ts
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
   host: "localhost",
-  user: "u569694274_slegacy",
+  user: "u569694274_abauer",
   password: "Mychildren*54",
   database: "u569694274_mylegacy",
 });
