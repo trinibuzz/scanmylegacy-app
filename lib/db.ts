6ab2af6e859adf3fbf993204b13ae27a661f6a78
trinibuzz <trinibuzzallmedia@gmail.com>
2026-04-23 22:28:20 -0400

    Fix DB host connection

--- lib/db.ts
+++ lib/db.ts
@@ -1,7 +1,7 @@
 import mysql from "mysql2/promise";
 
 export const db = mysql.createPool({
-  host: "localhost",
+  host: "mysql.hostinger.com",
   user: "u569694274_slegacy",
   password: "Mychildren*54",
   database: "u569694274_mylegacy",
