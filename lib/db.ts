c947613b6725af46b282688289e9f521ae73a862
trinibuzz <trinibuzzallmedia@gmail.com>
2026-04-24 03:33:51 -0400

    Updated DB password

--- lib/db.ts
+++ lib/db.ts
@@ -3,6 +3,6 @@ import mysql from "mysql2/promise";
 export const db = mysql.createPool({
   host: "mysql.hostinger.com",
   user: "u569694274_slegacy",
-  password: "Mychildren*54",
+  password: "Todayisgodday!54",
   database: "u569694274_mylegacy",
 });
